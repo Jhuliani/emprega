263a6e09065f0ab48b258924997275dca6a3fd35
Jhulaini <jhulianisantos@gmail.com>
2023-06-27 11:57:32 -0300

    inserindo compoente dinamicamente no formulario

--- src/app/cadastros/cadastro-form/cadastro-form.component.ts
+++ src/app/cadastros/cadastro-form/cadastro-form.component.ts
@@ -1,7 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Departamentos } from 'src/app/shared/models/departamentos.model';
 import { DepartamentosService } from '../../shared/services/departamentos.service';
 import { Senioridade } from 'src/app/shared/models/senioridade.model';
@@ -24,14 +23,9 @@ export class CadastroFormComponent implements OnInit {
   departamentos!: Observable<Departamentos[]>;
   niveis!: Observable<Senioridade[]>;
 
-  experienciasAcademicas: any[] = [];
-
-  experienciasProfissinais: any[] = [];
-
 
   constructor(
     private formBuilder: FormBuilder,
-    private http: HttpClient,
     private departamentosService: DepartamentosService,
     private senioridadeService: SenioridadeService,
     private verificaEmailService: VerificaEmailService,
@@ -44,8 +38,7 @@ export class CadastroFormComponent implements OnInit {
 
 
     this.departamentos = this.departamentosService.getDepartamentos();
-    this.niveis =  this.senioridadeService.getSenioridade();
-
+    this.niveis = this.senioridadeService.getSenioridade();
 
 
     this.formulario = this.formBuilder.group({
@@ -53,72 +46,90 @@ export class CadastroFormComponent implements OnInit {
       cabecalho: this.formBuilder.group({
         nome: [null, Validators.required],
         setor: [null, Validators.required],
-        nivel: [null, Validators.required],
+        senioridade: [null, Validators.required],
         cidade: [null, Validators.required],
         linkedin: [null, Validators.required],
         nascimento: [null, Validators.required],
         telefone: [null, Validators.required],
-        resumo: [null, Validators.required]
+        resumo: [null, Validators.required],
+        email: [null, [Validators.required, Validators.email], [this.validarEmail.bind(this)]],
       }),
-      // email: [null, [Validators.required, Validators.email], [this.validarEmail.bind(this)]],
-      // senha: [null, Validators.required]
-    })
 
+      formacao: this.formBuilder.array([]),
+      experienciaProf: this.formBuilder.array([])
 
-
+    });
 
 
   }
 
   onSubmit() {
+
     console.log(this.formulario.value);
 
     if (this.formulario) {
-      this.http.post('http://localhost:3000/curriculums', JSON.stringify({}))
-        .subscribe({
-          next: dados => {
-            console.log(dados);
-          },
-          error: error => {
-            alert('erro');
-          }
-        });
+      this.cadastroService.create(this.formulario.value).subscribe(
+        sucess => console.log('Sucesso'),
+        error => console.error('error')
+      );
     }
 
     else {
-        console.log('formulario invalido');
-
+      console.log('formulario invalido');
     }
-}
+  }
 
-
-
-  resetar(){
+  resetar() {
     this.formulario.reset();
   }
 
 
-  adicinonarExpAcademica(){
-    this.experienciasAcademicas.push({});
+  adicinonarExpAcademica(): void {
+    const expAcademica = this.formBuilder.group({
+      nomeCurso: [null, Validators.required],
+      dataInicio: [null, Validators.required],
+      dataFim: [null, Validators.required],
+      nomeInstituicao: [null, Validators.required]
+    });
+
+    const formacaoControl = this.formulario.get('formacao');
+    if (formacaoControl instanceof FormArray) {
+      formacaoControl.push(expAcademica);
+    }
+
   }
 
-  excluirExpAcademica(){
-    this.experienciasAcademicas.pop();
+  excluirExpAcademica(): void {
+    const formacao = this.formulario.get('formacao') as FormArray;
+    formacao.removeAt(formacao.length - 1);
   }
 
-  validarEmail(formControl: FormControl){
+  validarEmail(formControl: FormControl) {
     return this.verificaEmailService.verificarEmail(formControl.value)
-      .pipe(map(emailExiste => emailExiste ? {emailInvalido: true} : null));
+      .pipe(map(emailExiste => emailExiste ? { emailInvalido: true } : null));
   }
 
 
-  adicinonarExpProf(){
-    this.experienciasProfissinais.push({});
+  adicinonarExpProf() {
+    const expProfissional = this.formBuilder.group({
+      nomeCargo: [null, Validators.required],
+      dataInicio: [null, Validators.required],
+      dataFim: [null, Validators.required],
+      nomeEmpresa: [null, Validators.required]
+    });
+
+    const expProfControl = this.formulario.get('experienciaProf');
+    if (expProfControl instanceof FormArray) {
+      expProfControl.push(expProfissional);
+    }
   }
 
-  excluirExpProf(){
-    this.experienciasProfissinais.pop();
+  excluirExpProf() {
+    const experienciaProf = this.formulario.get('experienciaProf') as FormArray;
+    experienciaProf.removeAt(experienciaProf.length - 1);
   }
+
+
 
 }
 
